cd492f673ae0a41ec32c1dc1b3723b5f53d5a71d
dagreatcode <dagreatcode@gmail.com>
2020-10-22 19:01:34 -0400

    fixed my routes required the exC.js so in can move my gets into the routes in the eController

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const db = require("./models");
+const exerciseController = require("./controllers/exerciseController");
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 

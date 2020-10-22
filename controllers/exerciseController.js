ab787168c0a4dd8d674a3ee444310ed624bade9e
dagreatcode <dagreatcode@gmail.com>
2020-10-22 19:09:17 -0400

    from app to router

--- controllers/exerciseController.js
+++ controllers/exerciseController.js
@@ -1,4 +1,30 @@
 const express =  require("express");
 const router =  express.Router();
 
+router.get("/api/exercise", (req, res) => {
+    db.Exercise.find({}).then((foundExercise) => {
+        res.json(foundExercise);
+    }).catch((err) =>{
+        console.log(err);
+        res.json({
+            error: true,
+            data: null,
+            message: "Failed to retrieve Exercise.",
+        });
+    });
+});
+
+router.post("/api/exercise", (req, res) => {
+  db.Exercise.create(req.body).then((newExercise) => {
+      res.json(newExercise);
+  }).catch((err) =>{
+      console.log(err);
+      res.json({
+          error: true,
+          data: null,
+          message: "Failed to make new Exercise.",
+      });
+  });
+});
+
 modules.exports = router;
--- server.js
+++ server.js
@@ -36,30 +36,30 @@ mongoose.connect(
     });
   });
 
-  app.get("/api/exercise", (req, res) => {
-      db.Exercise.find({}).then((foundExercise) => {
-          res.json(foundExercise);
-      }).catch((err) =>{
-          console.log(err);
-          res.json({
-              error: true,
-              data: null,
-              message: "Failed to retrieve Exercise.",
-          });
-      });
-  });
+//   app.get("/api/exercise", (req, res) => {
+//       db.Exercise.find({}).then((foundExercise) => {
+//           res.json(foundExercise);
+//       }).catch((err) =>{
+//           console.log(err);
+//           res.json({
+//               error: true,
+//               data: null,
+//               message: "Failed to retrieve Exercise.",
+//           });
+//       });
+//   });
 
-  app.post("/api/exercise", (req, res) => {
-    db.Exercise.create(req.body).then((newExercise) => {
-        res.json(newExercise);
-    }).catch((err) =>{
-        console.log(err);
-        res.json({
-            error: true,
-            data: null,
-            message: "Failed to make new Exercise.",
-        });
-    });
-  });
+//   app.post("/api/exercise", (req, res) => {
+//     db.Exercise.create(req.body).then((newExercise) => {
+//         res.json(newExercise);
+//     }).catch((err) =>{
+//         console.log(err);
+//         res.json({
+//             error: true,
+//             data: null,
+//             message: "Failed to make new Exercise.",
+//         });
+//     });
+//   });
 
   app.listen(PORT, () => console.log("connected"));

e7f238728ea99e1b39e9f7194c5e94e950f66ffe
dagreatcode <dagreatcode@gmail.com>
2020-10-22 22:48:02 -0400

    fixing my route paths and stats

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -5,7 +5,15 @@ const db = require("../models");
 
 router.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
-})
+});
+
+router.get("/exercise", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.get("/stats", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
 
 router.get("/api/workouts", (req, res) => {
     db.Workout.find({}).then((foundWorkout) => {

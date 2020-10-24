0b80b1357682d5941df850b636fd0e2eab794945
dagreatcode <dagreatcode@gmail.com>
2020-10-23 23:36:24 -0400

    updating

--- models/workout.js
+++ models/workout.js
@@ -53,22 +53,3 @@ const Workout = mongoose.model("Workout", WorkoutSchema);
 
 
 module.exports = Workout;
-
-// const { Schema } = mongoose;
-// const blogSchema = new Schema({
-//     title:  String, // String is shorthand for {type: String}
-//     author: String,
-//     body:   String,
-//     comments: [{ body: String, date: Date }],
-//     date: { type: Date, default: Date.now },
-//     hidden: Boolean,
-//     meta: {
-//       votes: Number,
-//       favs:  Number
-//     }
-
-// const userSchema = new mongoose.Schema({
-//     // Make `name` required
-//     name: { type: String, required: true },
-//     age: Number
-//   });
